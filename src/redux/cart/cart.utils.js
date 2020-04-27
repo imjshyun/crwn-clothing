7127647c7c666798c6c1bdfd4cf64c30048ce6b4
James Hyun <imjshyun@gmail.com>
2020-04-27 19:56:42 +1000

    created cart-item component and connected our cart deopdown with cartItems reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,6 +13,6 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     );
   }
 
-  // 중복이 존재하지 않을시 quantity가 1인 새로운 array를 반환하여 항상 react 가 rerender하여 최신 정보를 얻게한다.
+  // 중복이 존재하지 않을시(새로운 아이템) 1을 값으로 갖는 quantity property를 추가하여 새로운 array를 반환하여 항상 react 가 rerender하여 최신 정보를 얻게한다.
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
