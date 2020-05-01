ec930d4da100be6d3f16c4679d97c8821218f2bc
James Hyun <imjshyun@gmail.com>
2020-05-01 14:54:32 +1000

    added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -18,6 +18,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         // cartItems: [...state.cartItems, action.payload], // 아이템 중복에 상관없이 추가
         cartItems: addItemToCart(state.cartItems, action.payload), // 아이템 중복을 피하고 추가
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload), // 아이템 중복을 피하고 추가
+      };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
       };
     default:
       return state;
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -16,3 +16,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   // 중복이 존재하지 않을시(새로운 아이템) 1을 값으로 갖는 quantity property를 추가하여 새로운 array를 반환하여 항상 react 가 rerender하여 최신 정보를 얻게한다.
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  // 1개 있을경우는 지울 아이템을 삭제
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
