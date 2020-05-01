b55153bdabd89f801e79646c62f4105823196b40
James Hyun <imjshyun@gmail.com>
2020-05-01 12:45:18 +1000

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -3,13 +3,19 @@ import { createSelector } from 'reselect';
 // 필요한 state를 선택
 const selectCart = (state) => state.cart;
 
-// 위에 선택된 state에서 필요한 것을 선택
+// 젤 위에 선택된 state에서 cartItems property를 선택
 export const selectCartItems = createSelector(
   [selectCart],
   (cart) => cart.cartItems
 );
 
-// 위에 cart state에서 선택된 cartItems을 이용.
+// 젤 위에 선택된 state에서 hidden property를 선택
+export const selectCartHidden = createSelector(
+  [selectCart],
+  (cart) => cart.hidden
+);
+
+// 위에 cart state에서 선택된 cartItems을 이용. interate하면서 total quantity를 계산하여 반환.
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   (cartItems) =>
@@ -18,4 +24,13 @@ export const selectCartItemsCount = createSelector(
         accumulatedQuantity + cartItem.quantity,
       0
     )
+);
+
+// 위에 cart state에서 선택된 cartItems을 이용. interate하면서 total price를 계산하여 반환.
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (accumulatedQuantity, cartItem) =>
+      accumulatedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
 );
