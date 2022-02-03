a7d6099b3776e7342365af02814b68d0fcaeb940
James <imjshyun@gmail.com>
2022-02-03 16:30:44 +1100

    update stripe integration and update our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,27 @@
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_VdNHnVYZ4Vg0kXSlZvGzAaOy';
+
+  const onToken = (token) => {
+    console.log(token);
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
